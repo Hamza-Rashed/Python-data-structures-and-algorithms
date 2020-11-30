17d03ed2a62e848cbe49cfa3042faaad17484c96
Hamza-Rashed <hamzarashed503@gmail.com>
2020-11-30 19:13:23 +0200

    done

--- data_structures_and_algorithms/challenges/array_shift/array_shift.py
+++ data_structures_and_algorithms/challenges/array_shift/array_shift.py
@@ -2,7 +2,7 @@ def insertShiftArray(arr,num):
     result = []
     for item in range(len(arr)):
         if arr[item] < num:
-           result.append(arr[item])
+           result.append(arr[item])#done
         elif arr[item] > num and arr[item-1] < num:
             result.append(num)
         else:
